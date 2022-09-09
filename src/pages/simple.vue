1092f5442ebe5c93edcaa854af21f663fe2ba1cd
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2022-09-09 08:39:54 +0200

    test affichage avec v-bind

--- src/pages/simple.vue
+++ src/pages/simple.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import Card from "../components/card.vue"
+const objet = {nom:"maison", prix:"43", nbrSDB:"2"}
 </script>
 <template>
     <Card nom="test" :prix="30" nbrSDB="3" favori="true"/>
+    <Card v-bind="objet"/>
 </template>
